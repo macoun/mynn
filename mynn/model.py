6dc971bb2a68cb1f0abd7a62280770740478d3b6
Ayaz, Ferhat <ferhat.ayaz@holisticon.de>
2021-01-08 20:04:43 +0100

    Added evaluation method and batch_size

--- mynn/model.py
+++ mynn/model.py
@@ -29,23 +29,54 @@ class Model:
             layer.backward(dinputs)
             dinputs = layer.dinputs
 
-    def train(self, X, y, epochs=1, print_every=1):
+    def evaluate(self, X, y, batch_size=32):
+        loss, acc, steps = 0, 0, 0
+        for batch_X, batch_y in self._batch(X, y, batch_size):
+            output = self.forward(batch_X)
+            loss += self.loss.calculate(output, batch_y)
+            predictions = self.layers[-1].predictions(output)
+            acc += self.accuracy.calculate(predictions, batch_y)
+            steps += 1
+        loss = loss / steps
+        acc = acc / steps
+        print(f'validation loss: {loss:.3f} acc: {acc:.3f}')
+
+    def train(self, X, y, epochs=1, print_every=1,
+              batch_size=32, validation_data=None):
         self.accuracy.init(y)
         trainable_layers = [l for l in self.layers if hasattr(l, 'weights')]
         for epoch in range(epochs):
-            output = self.forward(X)
-            self.backward(output, y)
+            loss, acc, steps = 0, 0, 0
+            for batch_X, batch_y in self._batch(X, y, batch_size):
+                output = self.forward(batch_X)
+                self.backward(output, batch_y)
 
-            loss = self.loss.calculate(output, y)
-            predictions = self.layers[-1].predictions(output)
-            accuracy = self.accuracy.calculate(predictions, y)
+                loss += self.loss.calculate(output, batch_y)
+                predictions = self.layers[-1].predictions(output)
+                acc += self.accuracy.calculate(predictions, batch_y)
 
+                self.optimizer.pre_update_params()
+                for layer in trainable_layers:
+                    self.optimizer.update_params(layer)
+                self.optimizer.post_update_params()
+
+                steps += 1
+
+            loss = loss / steps
+            acc = acc / steps
             if not epoch % print_every:
-                print(f'epoch: {epoch+1} acc: {accuracy} loss: {loss} '
+                print(f'epoch: {epoch+1} acc: {acc:.3f} loss: {loss:.3f} '
                       f'lr: {self.optimizer.current_learning_rate}')
-                pass
+                if validation_data is not None:
+                    self.evaluate(*validation_data, batch_size=32)
 
-            self.optimizer.pre_update_params()
-            for layer in trainable_layers:
-                self.optimizer.update_params(layer)
-            self.optimizer.post_update_params()
+    def _batch(self, X, y, batch_size):
+        if batch_size is None:
+            yield X, y
+        total_steps = len(X) // batch_size
+        if total_steps * batch_size < len(X):
+            total_steps += 1
+        for step in range(total_steps):
+            batch_X = X[step*batch_size:(step+1)*batch_size]
+            batch_y = y[step*batch_size:(step+1)*batch_size]
+            yield batch_X, batch_y
